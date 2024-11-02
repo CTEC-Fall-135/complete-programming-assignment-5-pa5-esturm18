723d956bb31eec8c1a20244aa77ba379599e6290
esturm18 <emma.sturm99@gmail.com>
2024-11-02 00:33:17 -0700

    Second push
    Might be final push...

--- Module5/LinkedList/LinkedList/Program.cs
+++ Module5/LinkedList/LinkedList/Program.cs
@@ -12,24 +12,19 @@
             //Initialize linkedlist
             LinkedList<string> list = new LinkedList<string>(rhyme);
 
+            Console.WriteLine("Linked list using foreach loop: "); 
             //Print out using enumerator
-            string sentence = ""; 
             foreach (string s in list)
             {
-                sentence += s + " "; 
-                //I concatenate the individual words into a single sentence
-                //Makes it so it prints on one line in stdout.
+                Console.Write(s + " ");  
             }
-            Console.WriteLine($"{sentence}");
 
             //Walk the linked list...
 
-            Console.WriteLine(" ");
-            Console.WriteLine("Walking the linked list"); 
+            Console.WriteLine("\n\nWalking the linked list:"); 
 
             LinkedListNode<string> node = list.First;
             LinkedListNode<string> savedNode = null; //Empty node to use later... 
-            string sentence1 = "";
             int nodeCount = 0; 
             while (node != null)
             {
@@ -39,15 +34,13 @@
             }
             //Remove a word from the middle of the list
 
-            Console.WriteLine(" ");
-            Console.Write("Removing..."); 
-            Console.WriteLine(" "); 
+            Console.WriteLine("\n\nRemoving the middle word: "); 
             //Start by getting count of number of nodes
 
             //Not worrying about if linked list is even versus odd length
             //Then divide that count by two to find where the middle node(s) might be 
             int length = nodeCount / 2;
-            int count = 1;
+            int count = 1; //Count to keep track of node count
 
             node = list.First; //Reset header node, currently at null...
             while (count <= length && node!= null)
@@ -55,26 +48,41 @@
                 count++;
                 node = node.Next;
             }
-            savedNode = node; 
+            savedNode = node; //Save the middle node (to be used again when adding back into the linked list). 
             list.Remove(node); //Remove the middle node
             node = list.First; //Reset the first node again, otherwise it will still be pointing to the middle word
 
-            string sentence3 = "";
-
             //Rewrite sentence without the middle word
-
             while (node != null)
             {
                 Console.Write(node.Value + " "); 
                 node = node.Next;
             }
 
-            Console.WriteLine(" ");
+            Console.WriteLine("\n");
 
             //Print again with word restored back to the list
             node = list.First; //Reset once again...
 
+            Console.WriteLine("Adding the middle word back in...");
+            count = 1; //Reset count
+            while (count < length)
+            {
+                node = node.Next; //Traverse through the nodes...
+                count++;
+            }
+            list.AddAfter(node, savedNode.Value); //Add in the middle word at the point it was removed  
+            
+            node = list.First; //Reset...sigh...again...
 
+           //Print out the rhyme again with middle work back in
+            while (node != null)
+            
+            {
+                Console.Write(node.Value + " ");
+                node = node.Next; 
+            }
+            Console.WriteLine("\n");
             #endregion
         }
     }
